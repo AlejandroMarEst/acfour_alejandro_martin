fa1420293685d726441349f701d35f29a8157b23
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2024-11-25 12:26:57 +0100

    ImplementaciC# Closes #3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace acfour_alejandro_martin
+{
+    public class Implementacio
+    {
+        public static void Main()
+        {
+            const string MSG1 = "Inserta el numero de numeros que vols insertar";
+            const string MSG2 = "Inserta el {0} numero:";
+            const string Error = "Error";
+            int numDeInputs = 0;
+            do
+            {
+                Console.WriteLine(MSG1);
+                numDeInputs = Convert.ToInt32(Console.ReadLine());
+            } while (numDeInputs <= 0);
+            int[] userNums = new int[numDeInputs];
+            try
+            {
+                for (int i = 0; i < userNums.Length; i++) {
+                    Console.WriteLine(MSG2);
+                    userNums(i) = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            catch (FormatException) {
+                Console.WriteLine(Error);
+            } 
+        }
+    }
+}
